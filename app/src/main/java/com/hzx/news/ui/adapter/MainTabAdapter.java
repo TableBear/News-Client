f4f6802d97b3f548a83f607cf72fda62fe579458
TableBear <2390445347@qq.com>
2019-04-08 00:36:21 +0800

    添加除Activity和Fragment之外的ui类文件

---
+++ app/src/main/java/com/hzx/news/ui/adapter/MainTabAdapter.java
@@ -0,0 +1,27 @@
+package com.hzx.news.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class MainTabAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public MainTabAdapter(List<Fragment> fragments, FragmentManager fm) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
---
+++ app/src/main/java/com/hzx/news/ui/uikit/GlideUtils.java
@@ -0,0 +1,48 @@
+package com.hzx.news.ui.uikit;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.hzx.news.R;
+
+public class GlideUtils {
+
+    public static void load(Context context, String url, ImageView iv) {
+        RequestOptions options = new RequestOptions();
+        options.placeholder(R.mipmap.ic_default);
+        Glide.with(context)
+                .load(url)
+                .apply(options)
+                .into(iv);
+    }
+
+    public static void load(Context context, String url, ImageView iv, int placeHolderResId) {
+        if (placeHolderResId == -1) {
+            Glide.with(context)
+                    .load(url)
+                    .into(iv);
+            return;
+        }
+        RequestOptions options = new RequestOptions();
+        options.placeholder(placeHolderResId);
+        Glide.with(context)
+                .load(url)
+                .apply(options)
+                .into(iv);
+    }
+
+    public static void loadRound(Context context, String url, ImageView iv) {
+        RequestOptions options = new RequestOptions();
+        options.placeholder(R.mipmap.ic_circle_default)
+                .centerCrop()
+                .circleCrop();
+
+        Glide.with(context)//
+                .load(url)//
+                .apply(options)//
+                .into(iv);
+    }
+
+}
